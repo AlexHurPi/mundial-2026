de4c3cdd26d8fc8aed2047fd25f67193661c657f
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2026-06-22 20:57:20 -0500

    commit5

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/mundial-2026/',
-  base: '/',
+  //base: '/mundial-2026/', //Asi es para desplegar en github
+  base: '/', //Asi es para desplegar en cloudflare
   // El "server.proxy" ya no es necesario porque hablas con tu propio backend en Vercel
   
   //---------------------------------------------------
