04d770f4f02ae4abfa2ee6de7f2eca0c3dccdb0c
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2026-06-25 20:09:49 -0500

    commit33

--- src/functions/resolutorLlaves.js
+++ src/functions/resolutorLlaves.js
@@ -1,70 +1,96 @@
-import { useState, useEffect } from 'react';
 
+const partidos16avos = [
+    {
+        hora: "28 jun, 14:00",
+        home: "Sudáfrica",
+        away: "Canadá"
+    },
+    {
+        hora: "29 jun, 12:00",
+        home: "Brasil",
+        away: "2º Grupo F"
+    },
+    {
+        hora: "29 jun, 15:30",
+        home: "Alemania",
+        away: "3º Grupo A/B/C/D/F"
+    },
+    {
+        hora: "29 jun, 20:00",
+        home: "1º Grupo F",
+        away: "Marruecos"
+    },
+    {
+        hora: "30 jun, 12:00",
+        home: "Costa de Marfil",
+        away: "2º Grupo I"
+    },
+    {
+        hora: "30 jun, 16:00",
+        home: "1º Grupo I",
+        away: "3º Grupo C/D/F/G/H"
+    },
+    {
+        hora: "30 jun, 20:00",
+        home: "México",
+        away: "3º Grupo A/E/H/I/J"
+    },
+    {
+        hora: "1 jul, 11:00",
+        home: "1º Grupo L",
+        away: "3º Grupo E/H/I/J/K"
+    },
+    {
+        hora: "1 jul, 15:00",
+        home: "1º Grupo G",
+        away: "3º Grupo A/E/H/I/J"
+    },
+    {
+        hora: "1 jul, 19:00",
+        home: "EE.UU",
+        away: "3º Grupo B/E/F/I/J"
+    },
+    {
+        hora: "2 jul, 14:00",
+        home: "1º Grupo H",
+        away: "2º Grupo J"
+    },
+    {
+        hora: "2 jul, 18:00",
+        home: "2º Grupo K",
+        away: "2º Grupo L"
+    },
+    {
+        hora: "2 jul, 22:00",
+        home: "Suiza",
+        away: "3º Grupo E/F/G/I/J"
+    },
+    {
+        hora: "3 jul, 13:00",
+        home: "2º Grupo D",
+        away: "2º Grupo G"
+    },
+    {
+        hora: "3 jul, 17:00",
+        home: "Argentina",
+        away: "2º Grupo H"
+    },
+    {
+        hora: "3 jul, 20:30",
+        home: "1º Grupo K",
+        away: "3º Grupo D/E/I/J/L"
+    }
+];
 
+export const resolverNombreFinales = (fecha, type)  => {
 
-export const resolverNombreFinales = (match, type) => {
-    const hora = match;
+    const partido = partidos16avos.find(
+        partido => partido.hora === fecha
+    );
 
+    if (!partido) {
+        return "Por definir";
+    }
 
-
-
-
-/*
-    // --- DOMINGO 28 DE JUNIO ---
-    if (hora === "28 jun, 14:00" && type === "home") return "Sudáfrica";
-    if (hora === "28 jun, 14:00" && type === "away") return "Canadá";
-
-    // --- LUNES 29 DE JUNIO ---
-    if (hora === "29 jun, 12:00" && type === "home") return "Brasil";
-    if (hora === "29 jun, 12:00" && type === "away") return "2º Grupo F";
-
-    if (hora === "29 jun, 15:30" && type === "home") return "Alemania";
-    if (hora === "29 jun, 15:30" && type === "away") return "3º Grupo A/B/C/D/F";
-
-    if (hora === "29 jun, 20:00" && type === "home") return "1º Grupo F";
-    if (hora === "29 jun, 20:00" && type === "away") return "Marruecos";
-
-    // --- MARTES 30 DE JUNIO ---
-    if (hora === "30 jun, 12:00" && type === "home") return "Costa de Marfil";
-    if (hora === "30 jun, 12:00" && type === "away") return "2º Grupo I";
-
-    if (hora === "30 jun, 16:00" && type === "home") return "1º Grupo I";
-    if (hora === "30 jun, 16:00" && type === "away") return "3º Grupo C/D/F/G/H";
-
-    if (hora === "30 jun, 20:00" && type === "home") return "México";
-    if (hora === "30 jun, 20:00" && type === "away") return "3º Grupo A/E/H/I/J";
-
-    // --- MIÉRCOLES 1 DE JULIO ---
-    if (hora === "1 jul, 11:00" && type === "home") return "1º Grupo L";
-    if (hora === "1 jul, 11:00" && type === "away") return "3º Grupo E/H/I/J/K";
-
-    if (hora === "1 jul, 15:00" && type === "home") return "1º Grupo G";
-    if (hora === "1 jul, 15:00" && type === "away") return "3º Grupo A/E/H/I/J";
-
-    if (hora === "1 jul, 19:00" && type === "home") return "EE.UU";
-    if (hora === "1 jul, 19:00" && type === "away") return "3º Grupo B/E/F/I/J";
-
-    // --- JUEVES 2 DE JULIO ---
-    if (hora === "2 jul, 14:00" && type === "home") return "1º Grupo H";
-    if (hora === "2 jul, 14:00" && type === "away") return "2º Grupo J";
-
-    if (hora === "2 jul, 18:00" && type === "home") return "2º Grupo K";
-    if (hora === "2 jul, 18:00" && type === "away") return "2º Grupo L";
-
-    if (hora === "2 jul, 22:00" && type === "home") return "Suiza";
-    if (hora === "2 jul, 22:00" && type === "away") return "3º Grupo E/F/G/I/J";
-
-    // --- VIERNES 3 DE JULIO ---
-    if (hora === "3 jul, 13:00" && type === "home") return "2º Grupo D";
-    if (hora === "3 jul, 13:00" && type === "away") return "2º Grupo G";
-
-    if (hora === "3 jul, 17:00" && type === "home") return "Argentina";
-    if (hora === "3 jul, 17:00" && type === "away") return "2º Grupo H";
-
-    if (hora === "3 jul, 20:30" && type === "home") return "1º Grupo K";
-    if (hora === "3 jul, 20:30" && type === "away") return "3º Grupo D/E/I/J/L";    
-
-
-    //console.log(match);
-    return "Por definir";*/
+    return partido[type];
 };
